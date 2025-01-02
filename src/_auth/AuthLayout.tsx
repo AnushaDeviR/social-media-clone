9d74fc0a3d682b9af008c8d7215a0ebbb1ce35d1
AnushaDeviR <anusha3599@gmail.com>
2025-01-02 08:16:53 +0000

    basic router-dom setup

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const AuthLayout = () => {
+    // will route both sign in and sign up forms; only components will be changed 
+  return (
+    <div>AuthLayout</div>
+  )
+}
